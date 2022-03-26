774bceaeffe1f1356815f7be9b60bc6ad176fa21
ykloo <65279675+ykloo@users.noreply.github.com>
2022-03-26 22:15:33 +0800

    created products api

---
+++ api/products/products.py
@@ -0,0 +1,89 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+dotenv_path = Path('../../config.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+ 
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = 'PRODUCT'
+ 
+    id = db.Column(db.String(13), primary_key=True)
+    product_name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.String(13), nullable=False)
+    description = db.Column(db.String(128))
+ 
+    def __init__(self, name, price, description):
+        self.id = id
+        self.product_name = product_name
+        self.price = price
+        self.description = description
+ 
+    def json(self):
+        return {"id": self.id, "name": self.product_name, "price": self.price, "description": self.description}
+
+@app.route("/product")
+def get_all():
+    product_list = Product.query.all()
+    if len(product_list):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "products": [product.json() for product in product_list]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no books."
+        }
+    ), 404
+
+
+@app.route("/product/<string:id>")
+def find_by_id(id):
+    product = Product.query.filter_by(id=id).first()
+    if product:
+        return jsonify(
+            {
+                "code": 200,
+                "data": product.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Product not found."
+        }
+    ), 404
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+ 
+                
