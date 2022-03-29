7fadb0aa59e53d0f0f877284a6e504c45be0200b
haziqilham <71216770+haziqilham@users.noreply.github.com>
2022-03-30 05:38:05 +0800

    WIP : Cart
    Keeps getting error 505.
    
    To recheck links

---
+++ api/products/cart.py
@@ -0,0 +1,73 @@
+import os
+
+# Comment this out during test
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask import Flask, render_template
+
+
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+ 
+app = Flask(__name__)
+CORS(app)
+
+# replace with link to test first 
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+
+
+class shoppingcart(db.Model):
+      __tablename__ = 'CART'
+
+      id = db.Column(db.VARCHAR(255), primary_key=True)
+
+      def __init__(self,id):
+             self.id = id
+    
+      def json(self):
+            return {"id": self.id}
+
+
+
+@app.route("/cartitems")
+def get_all():
+    cart_items = shoppingcart.query.all()
+    if len(cart_items):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "products": [product.json() for product in cart_items]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "No items in cart"
+        }
+    ), 404
+
+
+
+@app.route("/add/<string:id>", methods=['GET','POST'])
+def addToCart(id):
+#   productId =(request.args.get("productId"))
+
+  ID = shoppingcart(id)
+  db.session.add(ID)
+  db.session.commit
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+ 
+                
